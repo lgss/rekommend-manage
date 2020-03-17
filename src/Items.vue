f29121befb5beb4bbb37d016ce213aee1501897a
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2020-03-17 20:12:46 +0000

    added functionality to list and view journeys

--- src/Items.vue
+++ src/Items.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <v-navigation-drawer app absolute :clipped="true" color="blue--lighten-1">
-      <v-select solo label="Journey"></v-select>
+      <v-select :items="journeys" v-model="journey" item-text="label" item-value="id" label="Journey"  v-on:input="journeySelector(`${journey.id}`)"></v-select>
       <v-divider></v-divider>
+
       <v-list-group v-for="(page, index) in pages.pages" :key="index">
         <template v-slot:activator>
           <v-list-item-title @click="loadEditor(page, 'page')">Page {{index + 1}} - {{page.title}}</v-list-item-title>
@@ -18,21 +19,26 @@
             </v-list-item-action>
         </v-list-item>
       </v-list-group>
+
       <v-list-item color="primary" @click="newPage">
         <v-list-item-icon>
             <v-icon>mdi-plus</v-icon>
           </v-list-item-icon>
         New page
       </v-list-item>
+
       <v-list-item @click="copy">
         <v-list-item-title>Copy JSON</v-list-item-title>
       </v-list-item>
+
     </v-navigation-drawer>
+    
     <v-content>
       <v-container fluid class="fill-height">
         <component :is="interactionType" v-model="field"/>
       </v-container>
     </v-content>
+
   </div>
 </template>
 
@@ -52,20 +58,32 @@ export default {
   data() {
     return {
       pages: [],
+      journeys: [],
+      journey: {},
       item: 1,
       field: {fieldType: "div"},
       interactionType: ''
     }
   },
   created() {
-    fetch('https://1dds21470e.execute-api.eu-west-2.amazonaws.com/dev/test/1')
-      .then(x => x.json())
-      .then(x => {this.pages = x})
+    fetch('https://aqvneinxel.execute-api.eu-west-2.amazonaws.com/dev/journeys')
+      .then(y => y.json())
+      .then(y => {
+        this.journeys = y
+        })
       .finally(() => {
-        //this.loading = false
+        console.log(this.journeys)
       })
   },
   methods: {
+    journeySelector() {
+      fetch('https://aqvneinxel.execute-api.eu-west-2.amazonaws.com/dev/journeys/'+this.journey)
+        .then(x => x.json())
+        .then(x => {this.pages = x})
+        .finally(() => { 
+          //this.loading = false
+        })
+    },
     loadEditor(obj, fieldType) {
       this.field = obj
       this.interactionType = fieldType || obj.fieldType
