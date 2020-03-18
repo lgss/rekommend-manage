edab88080336c288b3bcaf54e97ca8db96f9e5c4
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2020-03-18 11:45:16 +0000

    add delete journey functionality

--- src/Items.vue
+++ src/Items.vue
@@ -3,44 +3,48 @@
     <v-navigation-drawer app absolute :clipped="true" color="blue--lighten-1">
       <v-select :items="journeys" v-model="journey" item-text="label" item-value="id" label="Journey"  v-on:input="journeySelector(`${journey.id}`)"></v-select>
       <v-divider></v-divider>
-
       <v-list-group v-for="(page, index) in pages.pages" :key="index">
         <template v-slot:activator>
           <v-list-item-title @click="loadEditor(page, 'page')">Page {{index + 1}} - {{page.title}}</v-list-item-title>
         </template>
-
         <v-list-item @click="loadEditor(item)" link v-for="(item, itemIndex) in page.items" :key="itemIndex">
           <v-list-item-title>{{item.fieldType}}</v-list-item-title>
           <v-list-item-subtitle>{{item.label}}</v-list-item-subtitle>
         </v-list-item>
       </v-list-group>
-
-      <v-list-item color="primary" @click="newPage">
-        <v-list-item-icon>
-            <v-icon>mdi-plus</v-icon>
+      <v-container v-if="journey !== ''">
+        <v-list-item color="primary" @click="newPage" >
+          <v-list-item-icon>
+              <v-icon>mdi-plus</v-icon>
+            </v-list-item-icon>
+          New page
+        </v-list-item>
+        <v-list-item @click="copy">
+          <v-list-item-icon>
+              <v-icon>mdi-content-copy</v-icon>
           </v-list-item-icon>
-        New page
-      </v-list-item>
-
+          Copy JSON
+        </v-list-item>
+        <v-list-item color="primary" @click="deleteJourney">
+          <v-list-item-icon>
+              <v-icon>mdi-delete</v-icon>
+            </v-list-item-icon>
+          Delete journey
+        </v-list-item>
+        <v-divider></v-divider>
+      </v-container>
       <v-list-item color="primary" @click="newJourney">
         <v-list-item-icon>
             <v-icon>mdi-plus</v-icon>
-          </v-list-item-icon>
+        </v-list-item-icon>
         New journey
       </v-list-item>
-
-      <v-list-item @click="copy">
-        <v-list-item-title>Copy JSON</v-list-item-title>
-      </v-list-item>
-
     </v-navigation-drawer>
-    
     <v-content>
       <v-container fluid class="fill-height">
         <component :is="interactionType" v-model="field"/>
       </v-container>
     </v-content>
-
   </div>
 </template>
 
@@ -61,7 +65,7 @@ export default {
     return {
       pages: [],
       journeys: [],
-      journey: {},
+      journey: '',
       item: 1,
       field: {fieldType: "div"},
       interactionType: ''
@@ -103,6 +107,18 @@ export default {
       })
         .then((res) => res.json())
         .then((data) =>  this.journeys.push(data))
+        .catch((err)=>console.error(err))
+    },
+    deleteJourney() {
+      fetch('https://aqvneinxel.execute-api.eu-west-2.amazonaws.com/dev/journeys/'+this.journey, {
+        method: 'DELETE'
+      })
+        .then((res) => res.json())
+        .then(() =>  {
+          let arr = this.journeys.filter((journey)=>{return journey.id !== this.journey})
+          this.journeys = arr
+          this.journey = ''
+        })
         .catch((err)=>console.error(err))
     }
   }
