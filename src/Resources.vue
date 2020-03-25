6a21520868a8590a8228b2ad9424185e39ac04b3
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2020-03-25 17:28:53 +0000

    Added layout for resources

--- src/Resources.vue
+++ src/Resources.vue
@@ -1,5 +1,12 @@
 <template>
-  <span>Resources config goes here</span>
+  <div>
+    <v-navigation-drawer app absolute :clipped="true" color="blue--lighten-1">
+      <p>list of resources</p>
+    </v-navigation-drawer>
+    <v-content>
+      <p>resource info</p>
+    </v-content>
+  </div>
 </template>
 
 <script>
