1d9852e977e6170467722a0035216722f8360f66
SturatParker <stuart.parker@cambridgeshire.gov.uk>
2020-04-08 14:24:11 +0100

    Resource editor component

---
+++ src/components/controls/ResourceEditor.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container>
+        <v-text-field label="Name" v-model="value.name"/>
+        <v-textarea label="Content" v-model="value.content"/>
+        
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'ResourceEditor',
+    props: ['value']
+
+}
+</script>
