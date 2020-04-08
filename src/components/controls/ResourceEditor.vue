3fba7fa0460b65b381cb7e11c5b86b68cbf955a6
SturatParker <stuart.parker@cambridgeshire.gov.uk>
2020-04-08 15:53:53 +0100

    tag support

--- src/components/controls/ResourceEditor.vue
+++ src/components/controls/ResourceEditor.vue
@@ -2,7 +2,22 @@
     <v-container>
         <v-text-field label="Name" v-model="value.name"/>
         <v-textarea label="Content" v-model="value.content"/>
-        
+        <v-combobox
+            v-model="value.includeTags"
+            chips
+            clearable
+            label="Include tags"
+            multiple
+            solo
+        />
+        <v-combobox
+            v-model="value.excludeTags"
+            chips
+            clearable
+            label="Exclude tags"
+            multiple
+            solo
+        />
     </v-container>
 </template>
 
