0783f88e797f24332b0e078ee18056b483e7bba6
SturatParker <stuart.parker@cambridgeshire.gov.uk>
2020-04-15 13:30:15 +0100

    Update Resources.vue

--- src/Resources.vue
+++ src/Resources.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
     <v-navigation-drawer app absolute :clipped="true" color="blue--lighten-1">
-      <v-list>
-        <v-select solo :items="resourceSets" v-model="currentResourceSetId" item-text="label" item-value="id" label="Resource"></v-select>
-        <v-list-item-group v-if="currentResourceSetId" v-model="resourceIndex" color="primary">
+      <v-list v-if="resources.length">
+        <!-- <v-select solo :items="resourceSets" v-model="currentResourceSetId" item-text="label" item-value="id" label="Resource"></v-select> -->
+        <v-list-item-group v-model="resourceIndex" color="primary"> -->
           <v-list-item
-            v-for="(resource, i) in currentResourceSet.doc" :key="i">
+            v-for="(resource, i) in resources" :key="i">
             <v-list-item-content>
-              <v-list-item-title v-html="resource.name"></v-list-item-title>
+              <v-list-item-title>Hello</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
-        </v-list-item-group>
+        </v-list-item-group> -->
       </v-list>
     </v-navigation-drawer>
     <v-content>
       <v-container fluid class="fill-height" v-if="currentResource">
         <v-btn @click="updateResources">Update Resources</v-btn>
-        <component :is="component" v-model="currentResource"/>
+        <!-- <component :is="component" v-model="currentResource"/> -->
       </v-container>
     </v-content>
   </div>
@@ -24,36 +24,24 @@
 
 
 <script>
-  import ResourceEditor from './components/controls/ResourceEditor'
+  // import ResourceEditor from './components/controls/ResourceEditor'
   
   export default {
     components :{
-      'resource-editor': ResourceEditor
+      // 'resource-editor': ResourceEditor
     },
     data: () => ({
-      resourceSets: [],
+      resources: [],
       resourceIndex: 0,
-      currentResourceSetId: null,
-      component: "resource-editor"
+      component: "resource-editor",
+      endpoint: ''
     }),
     created() {
-      fetch('https://aqvneinxel.execute-api.eu-west-2.amazonaws.com/dev/resources')
-      .then(response => response.json())
-      .then(rs => rs.map(r => {
-          r.doc = JSON.parse(r.doc).resources;
-          return r;
-      }))
-      .then(rs => {
-        this.resourceSets = rs;
-        if (this.resourceSets.length == 1) {
-          this.currentResourceSetId = this.resourceSets.find(Boolean).id;
-        }
-      })
+      fetch('https://0ynfhl6ndh.execute-api.eu-west-2.amazonaws.com/dev/resources')
+      .then(res => res.json())
+      .then(res => { this.resources = res })
     },
     computed: {
-      currentResourceSet() {
-        return this.resourceSets.find(set => set.id == this.currentResourceSetId)
-      },
       currentResource() {
         return this.currentResourceSet && this.currentResourceSet.doc ? this.currentResourceSet.doc[this.resourceIndex] : null
       }
