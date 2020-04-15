b7ac7ad53c54aa88dd3e992028e88ff1926f9b79
SturatParker <stuart.parker@cambridgeshire.gov.uk>
2020-04-15 16:18:10 +0100

    Update Resources.vue

--- src/Resources.vue
+++ src/Resources.vue
@@ -2,21 +2,19 @@
   <div>
     <v-navigation-drawer app absolute :clipped="true" color="blue--lighten-1">
       <v-list v-if="resources.length">
-        <!-- <v-select solo :items="resourceSets" v-model="currentResourceSetId" item-text="label" item-value="id" label="Resource"></v-select> -->
-        <v-list-item-group v-model="resourceIndex" color="primary"> -->
-          <v-list-item
-            v-for="(resource, i) in resources" :key="i">
+        <v-list-item-group v-model="resourceIndex" color="primary">
+          <v-list-item v-for="(resource, i) in resources" :key="i">
             <v-list-item-content>
-              <v-list-item-title>Hello</v-list-item-title>
+              <v-list-item-title v-if="resource.doc" v-html="resource.doc.name">Hello</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
-        </v-list-item-group> -->
+        </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
     <v-content>
       <v-container fluid class="fill-height" v-if="currentResource">
-        <v-btn @click="updateResources">Update Resources</v-btn>
-        <!-- <component :is="component" v-model="currentResource"/> -->
+        <v-btn @click="updateResource">Update Resources</v-btn>
+        <component :is="component" v-model="currentResource.doc"/>
       </v-container>
     </v-content>
   </div>
@@ -24,32 +22,46 @@
 
 
 <script>
-  // import ResourceEditor from './components/controls/ResourceEditor'
+  import ResourceEditor from './components/controls/ResourceEditor'
   
   export default {
     components :{
-      // 'resource-editor': ResourceEditor
+      'resource-editor': ResourceEditor
     },
     data: () => ({
       resources: [],
-      resourceIndex: 0,
+      resourceIndex: -1,
       component: "resource-editor",
-      endpoint: ''
+      endpoint: 'https://0ynfhl6ndh.execute-api.eu-west-2.amazonaws.com/dev'
     }),
     created() {
-      fetch('https://0ynfhl6ndh.execute-api.eu-west-2.amazonaws.com/dev/resources')
+      fetch(this.endpoint+'/resources')
       .then(res => res.json())
       .then(res => { this.resources = res })
     },
     computed: {
       currentResource() {
-        return this.currentResourceSet && this.currentResourceSet.doc ? this.currentResourceSet.doc[this.resourceIndex] : null
+        return this.resources.length && this.resourceIndex > -1 ? this.resources[this.resourceIndex] : null
       }
     },
     methods: {
-      updateResources() {
-        console.log("Update")
+      updateResource() {
+        let putReq = {
+          method: 'PUT',
+          body:JSON.stringify({
+            updates:[
+              {
+                paramName: "doc", paramValue: this.currentResource.doc
+              }
+            ]
+          })
+        }
+        console.log(putReq)
+        fetch(this.endpoint+'/resources/'+this.currentResource.id, putReq )
+        .then((res) => res.json())
+        .catch((err)=>console.error(err))
       }
     }
   }
+  
 </script>
