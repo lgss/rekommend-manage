c4dec4290b0fc2f961a60340dbe6988f57788584
Benedict <benedict@Benedicts-Work-MacBook-Pro.local>
2020-04-17 21:41:14 +0100

    Changed layout, commit to DB

--- src/General.vue
+++ src/General.vue
@@ -1,47 +1,49 @@
 <template> 
   <div >
-  <v-container id="home-preview">
-    <v-row>
-      <v-col>
-        <h1 class="float-left">LANDING PAGE PREVIEW</h1><br/>
-      </v-col>
-    </v-row>
-    <v-card>
-      <v-row>
-        <v-col>
-          <v-app-bar :color="color">
-            <v-toolbar-title class="white--text"><b>{{title}}</b></v-toolbar-title>
-          </v-app-bar>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col v-html="landing">
-          
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <v-btn id="btn-home-start-assessment">Go to assessment</v-btn>
-        </v-col>
-      </v-row>
-    </v-card>
-  </v-container>
   <v-container>
+    <v-row id="home-preview">
+      <v-col>
+        <v-row>
+          <v-col>
+            <h1 class="float-left">LANDING PAGE PREVIEW</h1><br/>
+          </v-col>
+        </v-row>
+          <v-card>
+            <v-row>
+              <v-col>
+                <v-app-bar :color="color">
+                  <v-toolbar-title class="white--text"><b>{{title}}</b></v-toolbar-title>
+                </v-app-bar>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col v-html="landing">
+                
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <v-btn id="btn-home-start-assessment">Go to assessment</v-btn>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
     <v-row>
       <v-col>
         <v-text-field label="Title" v-model="title"/> 
       </v-col>
-      <v-col :md="12">
-        <label>Theme colour</label>
-        <v-color-picker v-model="color"/>
-      </v-col>
       <v-col>
+        <label>Theme colour</label>
+        <v-color-picker mode="hexa" v-model="color"/>
+      </v-col>
+      <v-col md="12">
         <label>Landing content</label>
         <tiptap-vuetify v-model="landing" :extensions="extensions"/></v-col>
     </v-row>
     <v-row justify="center">
       <v-col>
-        <v-btn color="success">Save</v-btn>
+        <v-btn @click="save-general()" color="success">Save</v-btn>
       </v-col>
     </v-row>
   </v-container>
@@ -53,6 +55,16 @@
 import {TiptapVuetify, Heading, Bold, Italic, Strike, Underline, Paragraph, BulletList, OrderedList, ListItem, Link, Blockquote, HardBreak, HorizontalRule, History} from 'tiptap-vuetify'
 /* eslint-disable */
 export default {
+  created() {
+    fetch('https://nngfac1fjl.execute-api.eu-west-2.amazonaws.com/dev' + '/general')
+      .then(x => x.json())
+      .then(x => {
+        this.title = x.title
+        this.landing = x.landing
+        this.color = x.primary
+        this.loading = false
+      })
+  },
   components: {
     TiptapVuetify, 
     Heading, 
@@ -97,6 +109,21 @@ export default {
       color: "#1F63A3",
       landing: "",
       emptyResults: ""
+    }
+  },
+  methods: {
+    saveGeneral() {
+      fetch('https://nngfac1fjl.execute-api.eu-west-2.amazonaws.com/dev' + '/general', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          primary: this.color,
+          landing: this.landing,
+          title: this.title
+        })
+      })
     }
   }
 }
