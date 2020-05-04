42214226bdfd9229e255a3ee2fadbeae6b2dc189
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2020-05-04 11:51:55 +0100

    inital aut set up

---
+++ tests/test_editor.py
@@ -0,0 +1,13 @@
+import unittest
+import time
+import random
+from test_bootstrap import JerichoTest
+import json
+
+class EditorTests(JerichoTest):
+
+    def test_example(self):
+        self.browser.get(self.env["root"])
+
+        heading = self.browser.find_element_by_tag_name('h1')
+        self.assertEqual(heading.text, 'Rekommend Management', 'Title Check')
