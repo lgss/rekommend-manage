68bb26560226c7163ebae6e50901340448fee351
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2020-07-16 21:15:55 +0100

    Updated editor compontents for consistent preview

--- src/components/controls/TermsEditor.vue
+++ src/components/controls/TermsEditor.vue
@@ -2,77 +2,89 @@
     <v-container>
         <v-container v-if="loading"> </v-container>
         <v-container v-else>
-            <v-card id="terms-preview">
+            <v-row id="preview">
                 <v-col>
                     <v-row>
                         <v-col>
                             <h1 class="float-left">PREVIEW</h1>
-                            <br />
                         </v-col>
                     </v-row>
-                    <v-row>
-                        <v-col>
-                            <v-app-bar :color="appColor">
-                                <v-toolbar-title class="white--text"
-                                    ><b>{{ appTitle }}</b></v-toolbar-title
-                                >
-                            </v-app-bar>
-                        </v-col>
-                    </v-row>
-                    <v-container>
-                        <v-card>
-                            <div class="text-left">
-                                <v-container>
-                                    <h1 id="terms-title">{{ title }}</h1>
-                                </v-container>
-                                <v-divider />
-                                <v-container
-                                    id="terms-content"
-                                    class="text-left"
-                                    v-html="content"
-                                >
-                                </v-container>
-                            </div>
-                        </v-card>
-                    </v-container>
+                    <v-card>
+                        <v-row>
+                            <v-col>
+                                <v-app-bar :color="appPrimary" prominent>
+                                    <v-container class="d-flex flex-column align-center">
+                                        <a>
+                                            <v-toolbar-title class="white--text">
+                                                <div role="heading" aria-level="1" class="font-weight-black">{{ appTitle }}</div>
+                                            </v-toolbar-title>
+                                        </a>
+                                        <v-container class="d-flex flex-column align-center">
+                                            <h2 role="heading" aria-level="2" tabindex="0" class="white--text subheader">
+                                                Subheader
+                                            </h2>
+                                        </v-container>
+                                    </v-container>
+                                </v-app-bar>
+                            </v-col>
+                        </v-row>
+                        <v-row class="text-left">
+                            <v-container>
+                                    <h1 id="title">{{ title }}</h1>
+                            </v-container>
+                            <v-divider />
+                            <v-container
+                                id="content"
+                                class="text-left"
+                                v-html="content"
+                            ></v-container>
+                        </v-row>
+                        <v-footer :color="appSecondary" class="justify-center">
+                            <v-card flat tile :color="appSecondary">
+                                <v-card-text>
+                                    <a class="white--text">
+                                        Terms and Conditions
+                                    </a>
+                                </v-card-text>
+                            </v-card>
+                        </v-footer>
+                    </v-card>
                 </v-col>
-            </v-card>
-            <v-divider /> <br>
-            <v-card>
-				<v-container>
-                <v-row>
-                    <v-col md="12">
-                        <v-text-field label="Title" v-model="title" />
-                    </v-col>
-                    <v-col md="12">
-                        <html-editor :label="'Content'" v-model="content" />
-                    </v-col>
-                </v-row>
-
-                <v-row justify="center">
-                    <v-col>
-                        <v-btn @click="saveTerms" color="success">Save</v-btn>
-                    </v-col>
-                </v-row>
-				</v-container>
-            </v-card>
+            </v-row>
+            <v-row>
+                <v-col md="6">
+                    <v-text-field label="Title" v-model="title"/> 
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col md="12">
+                    <html-editor label="Content" v-model="content"/>
+                </v-col>
+            </v-row>
+            <v-row justify="center">
+                <v-col>
+                    <v-btn @click="saveTerms" color="success">Save</v-btn>
+                </v-col>
+            </v-row>
         </v-container>
     </v-container>
 </template>
 
 <script>
-import HTMLEditor from "./HTMLEditor"; /* eslint-disable */
+import HTMLEditor from "./HTMLEditor"; 
+/* eslint-disable */
 export default {
     name: "TermsEditor",
     created() {
         Promise.all([
-            fetch(this.endpoint + "/config/general")
+            fetch(this.endpoint + "/theme")
                 .then((x) => x.json())
                 .then((x) => {
                     this.appTitle = x.title;
-                    this.appColor = x.primary;
+                    this.appPrimary = x.primary;
+                    this.appSecondary = x.secondary;
                 }),
-            fetch(this.endpoint + "/config/disclaimer")
+            fetch(this.endpoint + "/content/disclaimer")
                 .then((x) => (x.ok ? x.json() : Promise.reject(x)))
                 .then((x) => {
                     this.title = x.title;
@@ -96,16 +108,17 @@ export default {
         return {
             endpoint: process.env.VUE_APP_API_ENDPOINT,
             method: "PUT",
-            title: null,
-            content: null,
             appTitle: null,
-            appColor: "#1F63A3",
+            appPrimary: "#1F63A3",
+            appSecondary: "#1F63A3",
+            title: "",
+            content: "",
             loading: true,
         };
     },
     methods: {
         saveTerms() {
-            fetch(this.endpoint + "/config/disclaimer", {
+            fetch(this.endpoint + "/content/disclaimer", {
                 method: "PUT",
                 headers: {
                     "Content-Type": "application/json",
@@ -116,18 +129,18 @@ export default {
                 }),
             });
         },
-    },
+    }
 };
 </script>
 <style scoped>
-#home-preview .v-card:hover {
-    opacity: 0.5;
-}
-#home-preview {
-    transform: matrix(0.75, 0, 0, 0.75, 0, 0);
-    text-align: center;
-    margin-top: 40px;
-    margin-left: 20px;
-    margin-right: 20px;
-}
+    #preview .v-card:hover {
+        opacity: 0.5;
+    }
+    #preview {
+        transform: matrix(0.75, 0, 0, 0.75, 0, 0);
+        text-align: center;
+        margin-top: 0px;
+        margin-left: 20px;
+        margin-right: 20px;
+    }
 </style>
