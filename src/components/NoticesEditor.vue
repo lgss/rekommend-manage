61d954227150e3140f15c83ba49166a6ebeb5165
Scott Phillips <30724358+ScottyTooHotty@users.noreply.github.com>
2020-10-14 13:16:28 +0100

    Added Notices Editor

---
+++ src/components/NoticesEditor.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h2>Notices</h2>
+        <div v-for="notice in notices" :key="notice">
+            <Notice :content="notice.content" @content="notice.content = $event">
+
+            </Notice>
+            {{notice.content}}
+            <v-btn @click="deleteNotice(notice.id)">Delete</v-btn>
+        </div>
+        <v-btn @click="addNotice">Add</v-btn>
+    </div>
+</template>
+
+<script>
+import {v4 as uuid} from "uuid"
+import Notice from '@/components/controls/Notice.vue'
+export default {
+    components: {
+        Notice
+    },
+    data() {
+        return {
+            notices: []
+        }
+    },
+    methods: {
+        addNotice() {
+            this.notices.push({
+                id: uuid(),
+                content: 'Hello world'
+            })
+        },
+        deleteNotice(id) {
+            const idx = this.notices.findIndex(x => x.id == id)
+            this.notices.splice(idx, 1)
+        }
+    }
+}
+</script>
---
+++ src/components/controls/Notice.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <HTMLEditor :value="content" @input="$emit('content', $event)">
+
+        </HTMLEditor>
+    </div>
+</template>
+
+<script>
+import HTMLEditor from  '@/components/controls/HTMLEditor.vue'
+export default {
+    components: {
+        HTMLEditor
+    },
+    props: ['content']
+}
+</script>
