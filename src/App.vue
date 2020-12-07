c4f3f30bebf29a18732eca97169e92828fd46234
James Pickett <james.pickett@cambridgeshire.gov.uk>
2020-12-07 19:40:34 +0000

    Load title from settings and removed "Latest release" link

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,7 @@
       color="primary"
       dark
     >
-      <v-toolbar-title>Rekommend Management</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://lgss.github.io/techknow"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-title>{{ process.env.TITLE }} editor</v-toolbar-title>
 
       <template v-slot:extension>
         <v-tabs grow v-model="tab">
