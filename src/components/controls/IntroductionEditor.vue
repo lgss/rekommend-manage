419ffd8a09e0c18bb47b597dc568b2c2283b2c63
Benedict <benedict@Benedicts-Work-MacBook-Pro.local>
2021-02-11 14:46:06 +0000

    Made headers more accessible
    - Removed headers from TOC
    - Changed top level header to title, lowered other headings down one level
    - Minor content tweaks

--- src/components/controls/IntroductionEditor.vue
+++ src/components/controls/IntroductionEditor.vue
@@ -17,55 +17,56 @@
             <v-row>
                 <v-col md="12">
                     <span>
-                        <h1>Contents</h1>
+                        <h1>Introduction</h1>
+                        <h2>Contents</h2>
                         <ol>
-                            <li><h2><a href="#general">General</a></h2></li>
+                            <li><a href="#general">General</a></li>
                                 <ul>
-                                    <li><h3><a href="#landing">Landing</a></h3></li>
-                                    <li><h3><a href="#completed">Assessment Completed</a></h3></li>
-                                    <li><h3><a href="#terms">Terms and Conditions</a></h3></li>
-                                    <li><h3><a href="#accessibility">Accessibility Statement</a></h3></li>
+                                    <li><a href="#landing">Landing</a></li>
+                                    <li><a href="#completed">Assessment Completed</a></li>
+                                    <li><a href="#terms">Terms and Conditions</a></li>
+                                    <li><a href="#accessibility">Accessibility Statement</a></li>
                                 </ul>
-                            <li><h2><a href="#journeycats">Journey Categories</a></h2></li>
-                            <li><h2><a href="#journeys">Journeys</a></h2></li>
+                            <li><a href="#journeycats">Journey Categories</a></li>
+                            <li><a href="#journeys">Journeys</a></li>
                                 <ul>
-                                    <li><h3><a href="#findedit">"How do I find and edit a journey?"</a></h3></li>
-                                    <li><h3><a href="#tags">Tags</a></h3></li>
-                                    <li><h3><a href="#wheretags">"Where are these tags?"</a></h3></li>
-                                    <li><h3><a href="#typetags">“What is the difference between types of tags?”</a></h3></li>
-                                    <li><h3><a href="#questiontags">Tags on questions:</a></h3></li>
-                                    <li><h3><a href="#answertags">Tags on answers:</a></h3></li>
-                                    <li><h3><a href="#resourcetags">Tags on resources:</a></h3></li>
+                                    <li><a href="#findedit">"How do I find and edit a journey?"</a></li>
+                                    <li><a href="#tags">Tags</a></li>
+                                    <li><a href="#wheretags">"Where are these tags?"</a></li>
+                                    <li><a href="#typetags">“What is the difference between types of tags?”</a></li>
+                                    <li><a href="#questiontags">Tags on questions:</a></li>
+                                    <li><a href="#answertags">Tags on answers:</a></li>
+                                    <li><a href="#resourcetags">Tags on resources:</a></li>
                                 </ul>
-                            <li><h2><a href="#resources">Resources</a></h2></li>
+                            <li><a href="#resources">Resources</a></li>
                         </ol>
                         <p></p>
-                        <a class="anchor" id="general"/><h1>General Tab</h1>
-                        <h2>‘Theme’</h2>
+                        <a class="anchor" id="general"/><h2>General tab</h2>
+                        <h3>‘Theme’</h3>
                         <p>The theme page controls the overarching design of the assessment side of TECHknow. It allows you to directly edit the website ‘Title’, the primary colour (header) and the secondary colour (footer).</p>
 
-                        <a class="anchor" id="landing"/><h2>‘Landing’</h2>
+                        <a class="anchor" id="landing"/><h3>‘Landing’</h3>
                         <p>The landing page allows you to directly edit the page in which every single user will first enter TECHknow and see. The page title and the body content which you wish user to read upon entry, and help them with their experience and what they should expect to find throughout the process.</p>
 
-                        <a class="anchor" id="completed"/><h2>‘Assessment Completed’</h2>
+                        <a class="anchor" id="completed"/><h3>‘Assessment Completed’</h3>
                         <p>This is the pop-up in which users will be presented with before any results are shown. This gives them the ability to decline viewing results or view their results based on answered submitted. You are able to change the content and title of this pop up using this page.</p>
 
-                        <a class="anchor" id="terms"/><h2>‘Terms and Conditions’</h2>
+                        <a class="anchor" id="terms"/><h3>‘Terms and Conditions’</h3>
                         <p>On this page you’ll be able to review and change the separate T’s & C’s page for the service located on every page of TECHknow in the footer.</p>
 
-                        <a class="anchor" id="accessibility"/><h2>‘Accessibility Statement’</h2>
+                        <a class="anchor" id="accessibility"/><h3>‘Accessibility Statement’</h3>
                         <p>The accessibility statement MUST be available to any user at any point during the service and provide up to date info on the state of accessibility/features available using TECHknow. This page sits in the footer of the assessment on every page and body content/title are editable here.</p>
 
-                        <a class="anchor" id="journeycats"/><h1>Journey Categories Tab</h1>
+                        <a class="anchor" id="journeycats"/><h2>Journey Categories tab</h2>
                         <p>When a user clicks ‘Start’ on their assessment they will be greeted with a question which requires an answer based on the area in which they need support. This ‘area’ selection page is controlled by the information within this tab. Add new answers/categories and tie these to Journey’s using this tab. You can select which ‘Journey(s)’ sit under what areas. Images for these categories can also be added at this point.</p>
 
-                        <a class="anchor" id="journeys"/><h1>‘Journeys’ Tab</h1>
+                        <a class="anchor" id="journeys"/><h2>Journeys tab</h2>
                         <p>Here you can create new journey’s or edit existing ones on the assessment.</p>
 
-                        <a class="anchor" id="findedit"/><h2>“How do I find and edit a journey?”</h2>
+                        <a class="anchor" id="findedit"/><h3>“How do I find and edit a journey?”</h3>
                         <p>Editing a journey is simple. Use the dropdown to view a list of all journey’s and select the one that you wish to edit. Pages within that journey will then be visible to view, delete or add information to. Journey’s consist of multiple pages. Click into pages in the column on the left hand side of the page to add, remove or amend any information on that particular part of the journey.</p>
 
-                        <a class="anchor" id="tags"/><h2>Tags</h2>
+                        <a class="anchor" id="tags"/><h3>Tags</h3>
                         <h3>“What are tags?”</h3>
                         <p>‘Tags’ are the most important part of the journey set up. All tags will help when identifying what resources need to be hidden from or shown to a user on their results page at the end of their assessment. A tag is an identifying word that can be tied to both a resource or an answer to a question in a journey for example, “Theft”. When a user answers a question with the word “Theft” tagged to it, the system will then look for all resources with that same tag.</p>
 
@@ -98,7 +99,7 @@
 
                         <h4>Exclude tags</h4><p> - When editing a resource these are used when you want to tag a resource to an answer which will enable it to HIDE in a user's results.</p>
 
-                        <a class="anchor" id="resources"/><h1>‘Resources’ Tab</h1>
+                        <a class="anchor" id="resources"/><h2>Resources tab</h2>
                         Resource can be a; 
                         <ul>
                             <li>Website</li>
